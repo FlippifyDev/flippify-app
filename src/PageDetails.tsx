536e0301d88b2d5dbb70b7f03627b208887a262b
BulletPopcorn1 <e.coiley@icloud.com>
2024-05-06 13:11:23 +0100

    Designed Title and Description

--- src/PageDetails.tsx
+++ src/PageDetails.tsx
@@ -3,10 +3,13 @@ import './styles/page-details.css'
 
 function PageDetails() {
     return (
-    <div className="page-details">
+    <div>
         <h1 className="Title">
             Flipping Made Easy.
         </h1>
+        <p className="Description">
+            Fast-track your profits with our lightning-quick deal bots, from lego to sneakers.
+        </p>
     </div>
     );
 }

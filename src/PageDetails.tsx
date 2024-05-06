bd5ffbe22269137dc5b5bf6bcabfd569d23b9bad
BulletPopcorn1 <e.coiley@icloud.com>
2024-05-06 12:32:54 +0100

    Updating Title

--- src/PageDetails.tsx
+++ src/PageDetails.tsx
@@ -2,7 +2,13 @@ import './styles/page-details.css'
 
 
 function PageDetails() {
-    return <div className="page-details"><h1>Flipping Made Easy.</h1></div>;
+    return (
+    <div className="page-details">
+        <h1 className="Title">
+            Flipping Made Easy.
+        </h1>
+    </div>
+    );
 }
 
 export default PageDetails;

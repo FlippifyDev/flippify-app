9c16edafef5a7ec73ca07ec03da49fd732e9a55d
RoyalGr4pe <njames.programming@gmail.com>
2024-06-21 16:22:20 +0100

    Login and Create account Buttons

--- app/l/components/Navbar.tsx
+++ app/l/components/Navbar.tsx
@@ -16,15 +16,15 @@ const Navbar = () => {
         <h1 className="text-3xl font-bold">Flippify</h1>
 
         <div role="tablist" className="tabs tabs-bordered">
-          <a href="/landing/home" className={`tab ${isActive('/landing/home')}`}>Home</a>
-          <a href="/landing/products" className={`tab ${isActive('/landing/products')}`}>Products</a>
-          <a href="/landing/about" className={`tab ${isActive('/landing/about')}`}>About</a>
+          <a href="/l/home" className={`tab ${isActive('/l/home')}`}>Home</a>
+          <a href="/l/products" className={`tab ${isActive('/l/products')}`}>Products</a>
+          <a href="/l/about" className={`tab ${isActive('/l/about')}`}>About</a>
           <a href="https://discord.com" className="tab">Discord</a>
         </div>
 
         <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
-          <li><a href="/landing/login">Login</a></li>
-          <li><a href="/landing/sign-up">Sign Up</a></li>
+          <li><a href="/l/login">Login</a></li>
+          <li><a href="/l/sign-up">Sign Up</a></li>
         </ul>
       </div>
       <hr className="my-1 h-px bg-gray-200 border-0 dark:bg-gray-700" />
--- app/l/sign-up/page.tsx
+++ app/l/sign-up/page.tsx
@@ -20,11 +20,12 @@ export default function SignUp() {
               <input type="text" className="grow" placeholder="Discord Username" />
             </label>
             <label className="input input-bordered flex items-center gap-2">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
-              <input type="password" className="grow" value="password" />
-            </label>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+            <input type="password" className="grow" placeholder="Password" />
+          </label>
           </div>
-
+          <button className="btn btn-outline btn-primary mt-3 w-40">Create Account</button>
+          <p className='mt-5'>Already have an account?<a  href="/l/login" className="link link-primary ml-2">Login</a></p>
         </div>
       </div>
     );

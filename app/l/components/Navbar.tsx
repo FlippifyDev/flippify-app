de518d70a8c54cecd563e474acc303e169717bcf
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2024-06-26 15:02:46 +0100

    All working, centered, discord login, user page. WORKING BACKUP

--- app/l/components/Navbar.tsx
+++ app/l/components/Navbar.tsx
@@ -13,25 +13,27 @@ const Navbar = () => {
 
 
   return (
-    <div className="navbar bg-base-100 flex justify-between items-center px-4 py-2">
-      <div className="flex items-center flex-1">
-        <h1 className="text-3xl font-bold">Flippify</h1>
-      </div>
-
-      <div role="tablist" className="tabs tabs-boxed">
-        <Link href="/l/home" className={`tab ${isActive('/l/home')}`}>Home</Link>
-        <Link href="/l/products" className={`tab ${isActive('/l/products')}`}>Products</Link>
-        <Link href="/l/about" className={`tab ${isActive('/l/about')}`}>About</Link>
-        <Link href="https://discord.gg/gNPYfe7YFm" className="tab">Discord</Link>
-      </div>
-
-        <div>
-          <SignInWithDiscord />
+    <div>
+      <div className="navbar bg-base-100 flex justify-between items-center px-4 py-2">
+        <div className="flex items-center flex-1">
+          <h1 className="text-3xl font-bold">Flippify</h1>
         </div>
 
+        <div role="tablist" className="tabs tabs-boxed">
+          <Link href="/l/home" className={`tab ${isActive('/l/home')}`}>Home</Link>
+          <Link href="/l/products" className={`tab ${isActive('/l/products')}`}>Products</Link>
+          <Link href="/l/about" className={`tab ${isActive('/l/about')}`}>About</Link>
+          <Link href="https://discord.gg/gNPYfe7YFm" className="tab">Discord</Link>
+        </div>
+
+        <div className="flex items-center justify-end flex-1">
+            <SignInWithDiscord />
+        </div>
       </div>
     </div>
   );
 };
 
 export default Navbar;
+
+

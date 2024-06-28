9fa3803c1d2bcff728a1d6172d48f4b622e1f2c4
RoyalGr4pe <royal.melon.dev@gmail.com>
2024-06-28 18:13:14 +0100

    prevent background from move and have it alway cover the screen

--- app/l/home/page.tsx
+++ app/l/home/page.tsx
@@ -6,9 +6,13 @@ import { Suspense } from 'react';
 export default function HomePage() {
   return (
     <Suspense fallback={<Loading />}>
-        <div>
-          <Navbar />
-          <HomeDetails />
+        <div className="h-screen w-screen flex flex-col">
+            <div className="fixed top-0 left-0 right-0 z-10">
+                <Navbar />
+            </div>
+            <div className="mt-16 flex-1 overflow-y-auto">
+                <HomeDetails />
+            </div>
         </div>
     </Suspense>
     

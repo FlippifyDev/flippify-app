60a0dd2510668d554612f89b8763666a8978e762
Alt5r <rrow8nn@gmail.com>
2024-06-28 16:05:12 +0100

    added margin so cards were not pushed all the way to the top of the page

--- app/u/components/Plans.tsx
+++ app/u/components/Plans.tsx
@@ -35,7 +35,7 @@ const Plans = () => {
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content">
             {/* Page content here */}
-            <div className="flex justify-center items-center p-4">
+            <div className="flex justify-center items-center p-4 mt-12">
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 <PlanCard image="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" title="Shoes" description="If a dog chews shoes whose shoes does he choose?"/>
                 <PlanCard image="https://images-na.ssl-images-amazon.com/images/I/91K5JOBomKL._SL1500_.jpg" title="Lego Retirement Sales" description="Monitor soon to retire lego sets across multiple websites."/>

c6149fd8636c2336224d841cbdcd0c5fb479becc
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2024-06-29 10:02:51 +0100

    fixed the clipping at the bottom of the header.

--- app/l/components/HomeDetails.tsx
+++ app/l/components/HomeDetails.tsx
@@ -4,7 +4,7 @@ const HomeDetails = ({ className = '' }) => {
     return (
         <div className={`home-details-container ${className}`}>    
             <div className='flex flex-col items-center mt-10'>
-                <p className='mb-8 text-5xl font-bold from-textGradStart to-textGradEnd bg-gradient-to-r bg-clip-text text-transparent'>Flipping<a className='mb-8 text-white text-5xl font-bold'> Made Easy.</a></p>
+                <p className='mb-8 text-5xl font-bold from-textGradStart to-textGradEnd bg-gradient-to-r bg-clip-text text-transparent py-1'>Flipping<a className='mb-8 text-white text-5xl font-bold'> Made Easy.</a></p>
                 <p className='mb-8 text-greyText text-lg'>Fast-track your profits with our lightning-quick deal bots, from lego to sneakers.</p>
                 <PhoneMockup />
                 <PhoneMockup />

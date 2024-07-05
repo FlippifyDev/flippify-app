8016947f1c22a51436da1fc2ed761637f22ac974
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2024-07-05 08:55:45 +0100

    The way i did smooth scroll was autistic so heres a better version

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
 import HomePage from './l/home/page'
 import Loading from './components/Loading';
-import { Suspense, useEffect } from 'react';
-import SmoothScroll from 'smooth-scroll';
-import '../styles/global.css';
+import { Suspense } from 'react';
+
 
 export default function Home() {
-  useEffect(() => {
-    const scroll = new SmoothScroll('a[href="#"]', {
-      speed: 800,
-      speedAsDuration: true,
-    });
-
-    return () => scroll.destroy();
-  }, []);
-
   return (
     <Suspense fallback={<Loading />}>
       <HomePage />

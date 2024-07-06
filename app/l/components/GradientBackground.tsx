fcfc59ce7ddfbaa857a92b9ec894d9049c5f18ed
RoyalGr4pe <royal.melon.dev@gmail.com>
2024-07-06 11:48:14 +0100

    remove while true from gradient bg

--- app/l/components/GradientBackground.tsx
+++ app/l/components/GradientBackground.tsx
@@ -1,37 +1,42 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, ReactNode } from 'react';
 import { motion, useAnimation } from 'framer-motion';
+import WhiteSection from "./WhiteSection";
+import Footer from "../components/Footer";
 
-const GradientBackground: React.FC = () => {
+interface GradientBackgroundProps {
+  children: ReactNode;
+}
+
+
+const GradientBackground: React.FC<GradientBackgroundProps> = ({ children }) => {
   const controls = useAnimation();
 
   useEffect(() => {
     const sequence = async () => {
-      while (true) {
-        try {
-          await controls.start({
-            background: [
-              'linear-gradient(120deg, #3E78B2 0%, #1B2A49 100%)',
-              'linear-gradient(120deg, #4E0D3A 0%, #040404 100%)',
-              'linear-gradient(120deg, #2B2D42 0%, #0A090C 100%)',
-              'linear-gradient(120deg, #1E3A8A 0%, #020024 100%)',
-              'linear-gradient(120deg, #4B0082 0%, #000000 100%)',
-              'linear-gradient(120deg, #4B0082 0%, #020024 100%)',
-              'linear-gradient(120deg, #1E3A8A 0%, #0A090C 100%)',
-              'linear-gradient(120deg, #2B2D42 0%, #040404 100%)',
-              'linear-gradient(120deg, #4E0D3A 0%, #1B2A49 100%)',
-              'linear-gradient(120deg, #3E78B2 0%, #1B2A49 100%)',
-            ],
-            transition: {
-              duration: 15,
-              ease: 'linear'
-            }
-          });
-          await new Promise(resolve => setTimeout(resolve, 15000)); // Wait for 15 seconds before starting the next animation
-        } catch (error) {
-          console.error('Error starting animation:', error);
-        }
+      try {
+        await controls.start({
+          background: [
+            'linear-gradient(120deg, #3E78B2 0%, #1B2A49 100%)',
+            'linear-gradient(120deg, #4E0D3A 0%, #040404 100%)',
+            'linear-gradient(120deg, #2B2D42 0%, #0A090C 100%)',
+            'linear-gradient(120deg, #1E3A8A 0%, #020024 100%)',
+            'linear-gradient(120deg, #4B0082 0%, #000000 100%)',
+            'linear-gradient(120deg, #4B0082 0%, #020024 100%)',
+            'linear-gradient(120deg, #1E3A8A 0%, #0A090C 100%)',
+            'linear-gradient(120deg, #2B2D42 0%, #040404 100%)',
+            'linear-gradient(120deg, #4E0D3A 0%, #1B2A49 100%)',
+            'linear-gradient(120deg, #3E78B2 0%, #1B2A49 100%)',
+          ],
+          transition: {
+            duration: 15,
+            ease: 'linear'
+          }
+        });
+        await new Promise(resolve => setTimeout(resolve, 15000)); // Wait for 15 seconds before starting the next animation
+      } catch (error) {
+        console.error('Error starting animation:', error);
       }
     };
 
@@ -40,9 +45,13 @@ const GradientBackground: React.FC = () => {
 
   return (
     <motion.div
-      className="fixed top-0 left-0 w-full h-full z-0"
+      className="relative top-0 left-0 w-full h-full"
       animate={controls}
-    />
+      >
+      <WhiteSection>
+        {children}
+      </WhiteSection>
+    </motion.div>
   );
 };
 

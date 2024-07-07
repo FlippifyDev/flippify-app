6c269a0a5207509c6af293044359708e8aede237
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2024-07-07 21:34:46 +0100

    Updating to deploy to netlify

--- app/u/[username]/dashboard/page.tsx
+++ app/u/[username]/dashboard/page.tsx
@@ -5,6 +5,7 @@ const DashboardPage = () => {
   return (
     <UserLayout>
       <div className="text-2xl text-white font-bold flex justify-center">Dashboard</div>
+      <div className="text-greyText flex justify-center">Coming Soon...</div>
     </UserLayout>
   );
 };

e08638951a991b07688f40572c11e00d1795959b
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2024-07-08 17:19:13 +0100

    Dashboard Coming Soon.

--- app/u/[username]/dashboard/page.tsx
+++ app/u/[username]/dashboard/page.tsx
@@ -4,8 +4,10 @@ import UserLayout from "../../components/UserLayout";
 const DashboardPage = () => {
   return (
     <UserLayout>
+      <div>
       <div className="text-2xl text-white font-bold flex justify-center">Dashboard</div>
       <div className="text-greyText flex justify-center">Coming Soon...</div>
+      </div>
     </UserLayout>
   );
 };

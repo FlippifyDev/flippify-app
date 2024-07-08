16117eba6624f1c48c65e0f522b6f123d9801c56
RoyalGr4pe <royal.melon.dev@gmail.com>
2024-07-08 10:01:43 +0100

    useEffect to loading screen

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,20 +1,25 @@
-import React from "react";
-import jokes from "./loading_messages"
+"use client"
+
+import React, { useEffect, useState } from "react";
+import jokes from "./loading_messages";
 import GradientBackground from "../l/components/GradientBackground";
 
-
 const Loading = () => {
-  const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
+  const [randomJoke, setRandomJoke] = useState("");
+
+  useEffect(() => {
+    setRandomJoke(jokes[Math.floor(Math.random() * jokes.length)]);
+  }, []);
 
   return (
-      <div>
-        <GradientBackground />
-        <div className="flex flex-col items-center justify-center min-h-screen">
-          <div className="mt-4 text-white text-lg font-semibold z-30">
-            {randomJoke}
-          </div>
-          <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-discordBlue"></div>
+    <div>
+      <GradientBackground />
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="mt-4 text-white text-lg font-semibold z-30">
+          {randomJoke}
         </div>
+        <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-discordBlue"></div>
+      </div>
     </div>
   );
 };

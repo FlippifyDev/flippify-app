932db8d8e6cfb8a4125bf948d464ef5063f3b8fd
RoyalGr4pe <njames.programming@gmail.com>
2024-07-12 21:28:15 +0100

    never found other verification in Head tag

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HomePage from './l/home/page'
 import Loading from './components/Loading';
 import { Suspense } from 'react';
 import { Metadata } from 'next'
+import Head from 'next/head';
 
 
 export const metadata: Metadata = {
@@ -12,6 +13,9 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div>
+      <Head>
+        <meta name="google-site-verification" content="Hho6-HQ44X7tDo2PgIXmXtPOFHsg069qvKUSqF3JfkE" />
+      </Head>
       <Suspense fallback={<Loading />}>
         <HomePage />
       </Suspense>

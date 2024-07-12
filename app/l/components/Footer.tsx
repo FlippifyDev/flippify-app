cfa86a57677d133f428296988ddcbccd5282299d
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2024-07-12 23:44:22 +0100

    Removed IG and Tiktok as we arent using them for now

--- app/l/components/Footer.tsx
+++ app/l/components/Footer.tsx
@@ -20,8 +20,8 @@ const Footer = () => {
           <Link href="legal" className="text-white hover:underline">Legal</Link>
         </aside>
         <nav className="flex items-center gap-4">
-          <Link href='https://www.instagram.com/flippifyuk/'><button className="mx-2 text-white"><FaInstagram className="text-2xl" /></button></Link>
-          <Link href='https://www.tiktok.com/@flippifyuk?lang=en'><button className="mx-2 text-white"><FaTiktok className="text-2xl" /></button></Link>
+          {/*<Link href='https://www.instagram.com/flippifyuk/'><button className="mx-2 text-white"><FaInstagram className="text-2xl" /></button></Link>
+          <Link href='https://www.tiktok.com/@flippifyuk?lang=en'><button className="mx-2 text-white"><FaTiktok className="text-2xl" /></button></Link>*/}
           <Link href='https://x.com/FlippifyUK'><button className="mx-2 text-white"><BsTwitterX className="text-2xl" /></button></Link>
           <Link href='https://discord.gg/gNPYfe7YFm'><button className="mx-2 text-white"><FaDiscord className="text-2xl" /></button></Link>
         </nav>
@@ -31,23 +31,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-/* 
-    <div className=" w-full select-none bg-white flex justify-center">
-      <footer className="footer flex flex-col md:flex-row items-center md:items-end justify-between p-4">
-        <aside className="flex items-center">
-          <a className={`${lato.className} text-black text-2xl font-bold`}>f</a>
-          <p className="text-black ml-2">Copyright © {new Date().getFullYear()} - All right reserved</p>
-        </aside>
-        <aside className="flex justify-center w-full md:w-auto mb-1 md:absolute md:left-1/2 md:transform md:-translate-x-1/2">
-          <Link href="legal" className="text-black hover:underline">Legal</Link>
-        </aside>
-        <nav className="flex items-center gap-4">
-          {/*<Link href='https://www.instagram.com/flippifyuk/'><button className="mx-2 text-black"><FaInstagram className="text-2xl" /></button></Link>
-          <Link href='https://www.tiktok.com/@flippifyuk?lang=en'><button className="mx-2 text-black"><FaTiktok className="text-2xl" /></button></Link>*/}
-          <Link href='https://x.com/FlippifyUK'><button className="mx-2 text-black"><BsTwitterX className="text-2xl" /></button></Link>
-          <Link href='https://discord.gg/gNPYfe7YFm'><button className="mx-2 text-black"><FaDiscord className="text-2xl" /></button></Link>
-        </nav>
-      </footer>
-    </div>
-*/

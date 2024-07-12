2375dd2aad15bbee4355c10c72305fbb329c6119
RoyalGr4pe <njames.programming@gmail.com>
2024-07-12 21:19:37 +0100

    google index test

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import HomePage from './l/home/page'
 import Loading from './components/Loading';
 import { Suspense } from 'react';
+import { Metadata } from 'next'
+import Head from 'next/head';
 
+
+export const metadata: Metadata = {
+  title: 'Flippify',
+  description: 'Reselling bots and tools. Powered by Stripe and Discord.'
+}
 
 export default function Home() {
   return (
-    <Suspense fallback={<Loading />}>
-      <HomePage />
-    </Suspense>
+    <div>
+      <Head>
+        <meta name="google-site-verification" content="613de683fca92c88" />
+      </Head>
+      <Suspense fallback={<Loading />}>
+        <HomePage />
+      </Suspense>
+    </div>
   );
 }

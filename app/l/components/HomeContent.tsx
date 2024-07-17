4fdaf9894c63742ea5049b69b44cc2c3643cfc56
RoyalGr4pe <njames.programming@gmail.com>
2024-07-18 00:01:34 +0100

    update title for smaller screens

--- app/l/components/HomeContent.tsx
+++ app/l/components/HomeContent.tsx
@@ -11,14 +11,14 @@ const inter = Inter({ subsets: ['latin'] });
 const HomeContent = ({ className = '' }) => {
   return (
     <div className={`home-details-container ${className}`}>
-      <div className='flex flex-col items-center space-y-3'>
+      <div className='flex flex-col items-center'>
         <div className="w-11/12 md:w-4/5 lg:w-3/5 xl:w-2/5">
           <p className={`${lato.className} text-5xl from-textGradStart to-textGradEnd to-60% bg-gradient-to-tr bg-clip-text text-transparent text-center`}>
             Reselling
-            <a href="#" className={`${inter.className} text-white text-5xl font-bold text-center`}> streamlined with cutting-edge bots & dedicated tools.</a>
+            <a href="#" className={`${inter.className} text-white text-5xl font-bold text-center`}> with cutting-edge bots and tools</a>
           </p>
         </div>
-        <p className='w-11/12 sm:w-full mb-8 pb-1 pt-2 text-greyText text-lg text-center'>
+        <p className='w-11/12 sm:w-full mt-3 mb-3 pb-1 pt-2 text-greyText text-lg text-center'>
           Providing An All-In-One Service Focused On Accelerating Your Reselling Profits.
         </p>
         <div className="flex items-center mb-3 pb-2">

997e12827f2f2d4323772b5c562f7e9770ab3afe
RoyalGr4pe <njames.programming@gmail.com>
2024-07-30 17:54:02 +0100

    Fix dashboard not displaying when user only has server subscription

--- app/u/[username]/dashboard/page.tsx
+++ app/u/[username]/dashboard/page.tsx
@@ -44,12 +44,33 @@ export default function Dashboard() {
       <ThemeSetter theme="light" />
       <Layout>
         <div className="w-full h-full">
-          <LayoutSubscriptionWrapper requiredSubscriptions={['standard']}>
+          {/* 
+            Please DO NOT CHANGE this logic right now, its a bit odd but works. 
+            A small change may result in the dashboard not displaying or may
+            result in the dashboard displaying twice.
+          */}
+
+          {/* Display dashboard if the user only has a standard subscription */}
+          <LayoutSubscriptionWrapper requiredSubscriptions={['standard', '!server']}>
             <DashboardPage />
           </LayoutSubscriptionWrapper>
+
+          {/* Display dashboard if the user only has a server subscription */}
+          <LayoutSubscriptionWrapper requiredSubscriptions={['server', '!standard']}>
+            <DashboardPage />
+          </LayoutSubscriptionWrapper>
+
+          {/* Display dashboard if the user has both standard and server subscription */}
+          <LayoutSubscriptionWrapper requiredSubscriptions={['server', 'standard']}>
+            <DashboardPage />
+          </LayoutSubscriptionWrapper>
+
+          {/* Display the waitlisted card if the user has not been whitelisted */}
           <LayoutSubscriptionWrapper requiredSubscriptions={['!whitelisted']}>
             <WaitlistContent />
           </LayoutSubscriptionWrapper>
+
+          {/* If the user is whitelisted but does not have a subscription then display the dashboard*/}
           <LayoutSubscriptionWrapper requiredSubscriptions={['whitelisted', '!standard', '!server']}>
             <DashboardPage />
           </LayoutSubscriptionWrapper>

bca07e19c8f7ac111e811f692fb3f9482dcfe3f0
RoyalGr4pe <njames.programming@gmail.com>
2024-07-30 17:32:32 +0100

    allow 0 in sales and profits and change icon

--- app/u/components/SalesTrackerTabAddPurchase.tsx
+++ app/u/components/SalesTrackerTabAddPurchase.tsx
@@ -88,7 +88,7 @@ const SalesTrackerTabAddPurchase: React.FC<SalesTrackerTabAddPurchaseProps> = ({
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (!parseFloat(e.key) && e.key !== 'Backspace' && e.key !== '.') {
+    if (!parseFloat(e.key) && e.key !== 'Backspace' && e.key !== '.' && e.key !== '0') {
       e.preventDefault();
     }
   };

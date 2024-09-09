9dcfad9a042f690a5d85bb21d88d41aeec214993
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2024-09-09 11:06:16 +0100

    Fixed spacing of icons on dashboard

--- app/u/components/home/dashboard/DashboardClickableBulletPoint.tsx
+++ app/u/components/home/dashboard/DashboardClickableBulletPoint.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IoMdCheckboxOutline } from 'react-icons/io';
+import { SiTicktick } from "react-icons/si";
 
 interface ClickableBulletPointProps {
   text: string;
@@ -10,7 +10,7 @@ const ClickableBulletPoint: React.FC<ClickableBulletPointProps> = ({ text, toolt
   return (
     <div className="relative group cursor-pointer">
       <div className="grid grid-cols-12 px-2">
-        <span className='2xl:col-span-1 col-span-2 flex items-center'><IoMdCheckboxOutline className="inline-block mr-3 text-houseBlue transition-transform duration-200 text-sm 2xl:text-base" /></span>
+        <span className='2xl:col-span-1 col-span-2 flex justify-end mr-3 items-center'><SiTicktick className="inline-block text-houseBlue transition-transform duration-200 text-sm 2xl:text-base" /></span>
         <span className="2xl:col-span-11 col-span-10 flex items-center font-semibold text-gray-400 group-hover:text-houseBlue transition-colors duration-200 select-none text-sm 2xl:text-base">{text}</span>
       </div>
       <div 

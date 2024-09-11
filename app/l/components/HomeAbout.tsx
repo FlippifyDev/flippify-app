e4450069614da9476a444c7e30eb0c83836e289d
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2024-09-11 23:41:14 +0100

    Doing the about section for the landing page - not finished yet

--- app/l/components/HomeAbout.tsx
+++ app/l/components/HomeAbout.tsx
@@ -1,76 +1,56 @@
-import React from 'react';
-import { HiBellAlert } from "react-icons/hi2";
-import { RiRobot2Fill } from "react-icons/ri";
-import {
-  MdOutlineSupportAgent,
-  MdUpdate,
-  MdOutlineSportsMartialArts,
-} from "react-icons/md";
-import { FaPeopleGroup } from "react-icons/fa6";
-import GetEarlyAccess from "./HomeGetEarlyAccess";
+import HomeAboutSeePlansButton from "./HomeAboutSeePlansButton";
+import ClickableBulletPoint from "./HomeClickableBulletPoint";
 
+import { Lato } from "next/font/google";
+import React from "react";
 
-const HomeAbout = () => { 
+const lato = Lato({ weight: "900", style: "italic", subsets: ["latin"] });
+
+const HomeAbout = () => {
   return (
-    <div id="about" className="flex flex-col items-center justify-center flex-grow mt-16 pt-28 pb-8 px-2">
-      <div className="bg-base-100 bg-opacity-98 p-8 mockup-shadow w-full max-w-4xl border-2 border-white text-center" style={{ borderRadius: '40px' }}>
-        <h1 className="text-2xl text-white font-bold mb-8">A Must-Have for resellers at every level to boost their profits</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-2">
-            <FeatureCard
-              title="Instant Alerts"
-              description="Real-time notifications for the latest deals directly in Discord."
-              icon={<HiBellAlert />}
-            />
-            <FeatureCard
-              title="Fully Automated Bots"
-              description="Seamless automation from web to Discord, saving time and boosting efficiency."
-              icon={<RiRobot2Fill />}
-            />
-            <FeatureCard
-              title="Constant Support"
-              description="We are always here to help ensuring you don't miss a thing."
-              icon={<MdOutlineSupportAgent />}
-            />
-            <FeatureCard
-              title="Community Driven"
-              description="Engage with a supportive community and share exclusive tips and deals."
-              icon={<FaPeopleGroup />}
-            />
-            <FeatureCard
-              title="Constant Updates"
-              description="Frequent bot updates to cover more websites and improve performance."
-              icon={<MdUpdate />}
-            />
-            <FeatureCard
-              title="Proven Expertise"
-              description="Years of experience in bot development, ensuring reliable and high-performing solutions."
-              icon={<MdOutlineSportsMartialArts />}
-            />
+    <div className="flex justify-center items-center h-full">
+      {/* Set to full height */}
+      <div className="w-full h-auto flex flex-col justify-center">
+        {/* Center the content vertically */}
+        <div className="text-center mx-4">
+          <h2 className={`${lato.className} text-4xl text-houseBlue font-bold mb-2`}>
+            Unlock Automated Profits
+          </h2>
+          <p className="text-gray-600 mb-4">
+            You&apos;re Just One Step Away from Automated Success. See what we have to offer!
+          </p>
+          <hr className="hr-price-stat mb-4" />
+          <div className="mb-6 w-full">
+            <h3 className="text-xl font-bold text-gray-700 mb-4">Why Choose Us?</h3>
+            <ul className="text-left space-y-2 min-w-xl">
+              {/* Adjust space between items */}
+              {[
+                { text: "24/7 Automated Deal-Finding Bots", tooltip: "Our advanced bots continuously scour the web for the most profitable deals across multiple categories, including electronics, LEGO, and more. Never miss a money-making opportunity, even while you sleep." },
+                { text: "Effortless Reselling Automation", tooltip: "From sourcing to order fulfillment, we handle every aspect of the reselling process. All you need to do is confirm the purchase and ship when it sells. Spend less time on tedious tasks and more time enjoying your profits." },
+                { text: "Monetize Your Server with Deal-Finding Bots", tooltip: "Transform your Discord server into a profitable reselling hub by integrating our deal-finding bots directly into your community. As a server owner, you can offer your users the same powerful tools and live alerts that our platform provides, without the hassle of creating or maintaining bots yourself. This means your members can access the best deals around the clock, generating revenue for themselves—and for you—while you sit back and let our bots do the work. Enhance your servers value, drive engagement, and earn passive income, all while offering your community top-tier reselling capabilities." },
+                { text: "Advanced Financial Tracking & Analytics", tooltip: " Stay on top of your business with our built-in financial management tools. Track profits, expenses, and inventory effortlessly, and make data-driven decisions to boost your bottom line." },
+                { text: "Exclusive Reseller Community, Events & Expert Support", tooltip: "Become part of a thriving community of successful resellers with access to exclusive events, workshops, and giveaways. We regularly host live Q&A sessions with industry specialists, offer hands-on workshops, and conduct exciting giveaways to keep you engaged and growing. Plus, we’re continually expanding our team of experts and developing a comprehensive course designed to help both new and seasoned resellers sharpen their skills and scale their business. Whether you’re just starting out or looking to take your reselling to the next level, our community offers the tools, insights, and support you need to succeed." },
+                { text: "Continual System Enhancements", tooltip: "We regularly update our bots and tools with new features and improvements. As our service evolves, so does your advantage in the marketplace." },
+                { text: "Ad-Free, Streamlined Experience", tooltip: "Enjoy an uninterrupted, ad-free experience that allows you to focus on what matters most - growing your reselling business." },
+                { text: "Early Access to New Tools and Features", tooltip: "Be the first to try out new tools and updates. Stay ahead of the curve with features that keep you competitive in the fast-paced world of reselling." },
+                { text: "Self-Promotion and Networking Opportunities", tooltip: "Leverage our platform to promote your own services or products and connect with other like-minded entrepreneurs. Expand your reach and grow your network." }
+              ].map((item, index) => (
+                <li key={index} className="w-full">
+                  <ClickableBulletPoint 
+                    text={item.text}
+                    tooltip={item.tooltip}
+                  />
+                </li>
+              ))}
+            </ul>
           </div>
-            <div className="pt-5 flex justify-center w-auto">
-              <GetEarlyAccess />
-            </div>
+          <div className="border-t border-gray-200 pt-6">
+            <HomeAboutSeePlansButton />
+          </div>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default HomeAbout
-
-function FeatureCard({
-    title,
-    description,
-    icon,
-  }: {
-    title: string;
-    description: string;
-    icon: React.ReactNode;
-  }) {
-    return (
-      <div className="flex flex-col items-center text-center p-4 bg-transparent rounded-lg shadow">
-        <h2 className="text-white text-2xl">{icon}</h2>
-        <h2 className="text-lg font-semibold mb-2 text-white">{title}</h2>
-        <p className="text-greyText text-md">{description}</p>
-      </div>
-    );
-  }
+export default HomeAbout;
--- app/l/components/HomeContent.tsx
+++ app/l/components/HomeContent.tsx
@@ -3,6 +3,7 @@ import HomeGetEarlyAccess from './HomeGetEarlyAccess';
 import HomeAbout from './HomeAbout';
 import HomeMockupPhone from './HomeMockupPhone';
 import HomeMockupBrowser from './HomeMockupBrowser';
+import HomeShowcase from './HomeShowcase';
 
 const lato = Lato({ weight: '900', style: 'italic', subsets: ['latin'] });
 const inter = Inter({ subsets: ['latin'] });
@@ -13,15 +14,25 @@ const HomeContent = ({ className = '' }) => {
       <div className='flex flex-col items-center'>
         <div className="w-11/12 mt-3 lg:mt-10 md:w-4/5 lg:w-3/5 xl:w-2/5">
           <p className={`${lato.className} text-6xl text-white text-center`}>
-            Automated Reselling using <span className="bg-gradient-to-tr pb-1 from-[#35B4F8] to-[#81EAFA] bg-clip-text text-transparent">AI</span>
+            Automated Reselling using <span className="bg-gradient-to-tr pb-1 from-textGradStart to-textGradEnd bg-clip-text text-transparent">AI</span>
           </p>
         </div>
         <p className='w-11/12 sm:w-full mt-3 mb-8 pb-1 pt-2 text-gray-300 text-lg text-center'>
-          We handle everything so you don't have to, eBay & Amazon store automation & tracking in all areas.
+          Handling everything so you don't have to, eBay & Amazon store automation & tracking in all areas.
         </p>
         
         <HomeGetEarlyAccess />
-        <HomeAbout />
+        
+        <div className="pt-[90px]" />
+        <div className="flex flex-col lg:flex-row my-4 md:my-11 mx-2 md:mx-6 py-8 sm:py-2 md:py-0 md:pl-4 bg-white bg-opacity-85 border border-gray-300 rounded-3xl shadow-md overflow-hidden">
+          <div className="lg:w-1/3">
+            <HomeAbout />
+          </div>
+          <div className="lg:w-2/3">
+            <HomeShowcase />
+          </div>
+        </div>
+
         <HomeMockupPhone />
         <HomeMockupBrowser />
       </div>

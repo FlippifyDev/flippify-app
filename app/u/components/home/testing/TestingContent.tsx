9cefd61abfbdb0761b3fcc5dca58ad964ea83b1d
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2024-09-24 10:04:11 +0100

    Test ebay connection set up

--- app/u/components/home/testing/TestingContent.tsx
+++ app/u/components/home/testing/TestingContent.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ControlNotificationsButton from "./ControlNotificationsButton";
 import TestNotificationButton from "./TestNotificationButton";
+import EbayConnect from "./EbayConnect"
 
 const testContent = () => {
   return (
     <div className="w-full mb-4">
-      <div className="w-full mb-4">Notifications Testing</div>
+      <div className="mb-4">Notifications Testing</div>
       <div className="w-full mb-10 flex justify-start gap-4">
         <ControlNotificationsButton />
         <TestNotificationButton />
       </div>
-      <div>
+      <div className="mb-4">
         eBay and Amazon Connection Testing here
+      </div>
+      <div className="w-full mb-10 flex justify-start">
+        <EbayConnect />
       </div>
     </div>
   );

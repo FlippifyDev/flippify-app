8ec6661b4b91ea1cc71ef30b7bd1806dfa4d1dd1
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2024-10-03 11:09:29 +0100

    Fixed sign out button, hopefully fixed sidebar?

--- app/u/components/dom/sidebar/SidebarSignOutButton.tsx
+++ app/u/components/dom/sidebar/SidebarSignOutButton.tsx
@@ -24,11 +24,11 @@ const SidebarSignOutButton = () => {
 
   return (
     <button
-      className="text-lightModeText grid grid-cols-12 items-center gap-2 px-4 py-2 rounded-md transform transition duration-200 hover:bg-red-600 hover:text-white"
+      className="text-lightModeText grid grid-cols-12 items-center gap-2 px-4 py-2 rounded-md w-full transition duration-200 hover:bg-red-600 hover:text-white"
       onClick={handleSignOut}
     >
       <span className="col-span-2 text-lg"><FaSignOutAlt /></span>
-      <span className="col-span-10 text-base text-left ml-4">Sign Out</span>
+      <span className="col-span-10 text-base text-left">Sign Out</span>
     </button>
   );
 };

b6cf8f3e62a8d29c9faad11dbc80f456f6c94b04
RoyalGr4pe <royal.melon.dev@gmail.com>
2024-10-17 18:47:20 +0100

    attempt at sidebar and profile dropdown fix

--- app/u/components/dom/navbar/NavbarProfileAvatar.tsx
+++ app/u/components/dom/navbar/NavbarProfileAvatar.tsx
@@ -15,8 +15,7 @@ const NavbarProfileAvatar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   // Default avatar
-  let avatar =
-    "https://i.pinimg.com/originals/40/a4/59/40a4592d0e7f4dc067ec0cdc24e038b9.png";
+  let avatar = "https://i.pinimg.com/originals/40/a4/59/40a4592d0e7f4dc067ec0cdc24e038b9.png";
   let referral_code = "None";
 
   if (session) {
@@ -58,6 +57,7 @@ const NavbarProfileAvatar = () => {
     fetchCheckoutUrl();
   }, [session]);
 
+
   const handleBillingPortalButtonClick = () => {
     if (billingUrl) {
       window.open(billingUrl, "_blank");
@@ -65,7 +65,6 @@ const NavbarProfileAvatar = () => {
   };
 
   const handleProfileOpen = (e: React.MouseEvent) => {
-    e.stopPropagation();
     if (session) {
       if (session.user?.name) {
         router.push(`/u/${session.user.name}/profile`);
@@ -76,11 +75,25 @@ const NavbarProfileAvatar = () => {
   };
 
   const toggleDropdown = () => {
-    setIsDropdownOpen((prev) => !prev);
+    setIsDropdownOpen((prevState) => !prevState); // Toggles the dropdown state
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.dropdown')) {
+      setIsDropdownOpen(false); // Closes dropdown when clicking outside
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      console.log("Cleaning up, removing event listener");
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   const handleAdminOpen = (e: React.MouseEvent) => {
-    e.stopPropagation();
     if (session) {
       if (session.user?.name) {
         router.push(`/u/${session.user.name}/asldf0987asDa230fDsADMIN`);
@@ -91,7 +104,6 @@ const NavbarProfileAvatar = () => {
   };
 
   const handleTestingOpen = (e: React.MouseEvent) => {
-    e.stopPropagation();
     if (session) {
       if (session.user?.name) {
         router.push(`/u/${session.user.name}/bfoau214QNI42nAjTEST`);
@@ -107,7 +119,7 @@ const NavbarProfileAvatar = () => {
         tabIndex={0}
         role="button"
         className="btn btn-ghost btn-circle avatar"
-        onClick={toggleDropdown}
+        onClick={toggleDropdown} // Toggle dropdown on avatar click
       >
         <div className="w-10 rounded-full">
           <Image
@@ -119,56 +131,39 @@ const NavbarProfileAvatar = () => {
           />
         </div>
       </div>
-
-      {/* Decide to open / close menu */}
-      {isDropdownOpen && (
+      {isDropdownOpen && ( // Conditionally render the dropdown menu
         <ul
+          tabIndex={0}
           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          onClick={(e) => e.stopPropagation()} // Prevents closing when clicking on the menu
         >
-          <button
-            className="relative flex flex-col flex-wrap flex-shrink-0 align-items rounded-md hover:bg-gray-200 active:bg-gray-300 transform transition duration-200"
-            onClick={handleProfileOpen}
-          >
-            <span className="text-start px-[0.75rem] py-[0.25rem] text-[0.875rem]">
-              Profile
-            </span>
+          <button 
+            className='relative flex flex-col flex-wrap flex-shrink-0 align-items rounded-md hover:bg-gray-200 active:bg-gray-300 transform transition duration-200' 
+            onClick={handleProfileOpen}>
+            <span className='text-start px-[0.75rem] py-[0.25rem] text-[0.875rem]'>Profile</span>
           </button>
-          <LayoutSubscriptionWrapper requiredSubscriptions={["admin"]}>
-            <button
-              className="relative flex flex-col flex-wrap flex-shrink-0 align-items rounded-md hover:bg-gray-200 active:bg-gray-300 transform transition duration-200"
-              onClick={handleAdminOpen}
-            >
-              <span className="text-start px-[0.75rem] py-[0.25rem] text-[0.875rem]">
-                Admin
-              </span>
+          <LayoutSubscriptionWrapper requiredSubscriptions={['admin']}>
+            <button 
+              className='relative flex flex-col flex-wrap flex-shrink-0 align-items rounded-md hover:bg-gray-200 active:bg-gray-300 transform transition duration-200' 
+              onClick={handleAdminOpen}>
+              <span className='text-start px-[0.75rem] py-[0.25rem] text-[0.875rem]'>Admin</span>
             </button>
           </LayoutSubscriptionWrapper>
-          <LayoutSubscriptionWrapper requiredSubscriptions={["admin"]}>
-            <button
-              className="relative flex flex-col flex-wrap flex-shrink-0 align-items rounded-md hover:bg-gray-200 active:bg-gray-300 transform transition duration-200"
-              onClick={handleTestingOpen}
-            >
-              <span className="text-start px-[0.75rem] py-[0.25rem] text-[0.875rem]">
-                Testing Area
-              </span>
+          <LayoutSubscriptionWrapper requiredSubscriptions={['admin']}>
+            <button 
+              className='relative flex flex-col flex-wrap flex-shrink-0 align-items rounded-md hover:bg-gray-200 active:bg-gray-300 transform transition duration-200' 
+              onClick={handleTestingOpen}>
+              <span className='text-start px-[0.75rem] py-[0.25rem] text-[0.875rem]'>Testing Area</span>
             </button>
           </LayoutSubscriptionWrapper>
-          <button
-            className="relative flex flex-col flex-wrap flex-shrink-0 align-items rounded-md hover:bg-gray-200 active:bg-gray-300 transform transition duration-200"
-            onClick={handleBillingPortalButtonClick}
-          >
-            <span className="text-start px-[0.75rem] py-[0.25rem] text-[0.875rem]">
-              Billing Portal
-            </span>
+          <button 
+            className='relative flex flex-col flex-wrap flex-shrink-0 align-items rounded-md hover:bg-gray-200 active:bg-gray-300 transform transition duration-200' 
+            onClick={handleBillingPortalButtonClick}>
+            <span className='text-start px-[0.75rem] py-[0.25rem] text-[0.875rem]'>Billing Portal</span>
           </button>
-          <button
-            className="relative flex flex-col flex-wrap flex-shrink-0 align-items rounded-md hover:bg-red-600 hover:text-white active:bg-red-700 transform transition duration-200"
-            onClick={handleSignOut}
-          >
-            <span className="text-start px-[0.75rem] py-[0.25rem] text-[0.875rem]">
-              Sign Out
-            </span>
+          <button 
+            className='relative flex flex-col flex-wrap flex-shrink-0 align-items rounded-md hover:bg-red-600 hover:text-white active:bg-red-700 transform transition duration-200' 
+            onClick={handleSignOut}>
+            <span className='text-start px-[0.75rem] py-[0.25rem] text-[0.875rem]'>Sign Out</span>
           </button>
         </ul>
       )}

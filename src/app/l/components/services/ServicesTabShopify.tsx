d2339f340b016b612791e0197e775e3f7d9cea43
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2025-03-13 12:58:07 +0000

    Moved images to public instead of fetching from imgur

--- src/app/l/components/services/ServicesTabShopify.tsx
+++ src/app/l/components/services/ServicesTabShopify.tsx
@@ -13,68 +13,68 @@ const ShopifyServices: React.FC = () => {
 					<p
 						className={`${lato.className} text-5xl from-textGradStart to-textGradEnd to-60% bg-gradient-to-tr bg-clip-text text-transparent py-1`}
 					>
-						Tools
 						<a
 							className={`${inter.className} mb-8 text-white text-5xl font-bold`}
 						>
-							{/* This is a space */} Selection
+							Shopify Automation
 						</a>
+						{/* This is a space */} Services
 					</p>
 				</div>
 				<div className="flex justify-center max-w-2xl">
 					<p className="mx-4 sm:mx-2 text-white text-md sm:text-lg text-center">
-						Uncover a handpicked collection of tools designed to simplify tasks and improve workflow efficiency, ensuring you stay ahead in today&apos;s competitive market.
+					Automate your Shopify store with AI-driven tools for product listing, order fulfillment, inventory management, and financial tracking, helping you scale effortlessly.
 					</p>
 				</div>
 			</div>
 			<div className="flex flex-wrap justify-center ">
 				<ServicesToolsCard
-					title="Auto Deal Finding"
-					description="Discover unbeatable sales with our Smart Deal Finder! Our bots do all the hard work for you, constantly searching thousands of products over hundreds of websites. Our monitors provides real-time updates and detailed comparisons to help you find the best deals available."
+					title="Listing Automation"
+					description="Create optimized eBay listings instantly with the use of our browser extension or by pasting a product link. AI extracts product details, generates a title, description, and pricing, and posts the listing automatically."
 					disclaimer={null}
-					image="https://i.imgur.com/uXqxOZm.png"
+					image="/ListingAutomationToolBanner.png"
 				/>
 				<ServicesToolsCard
-					title="Inventory Tracking"
+					title="Smart Pricing Optimization"
+					description="Stay competitive with AI-driven pricing that adjusts automatically based on the products market trends, competitor listings, and sales performance to maximize profitability."
+					disclaimer={null}
+					image="/PriceOptimizationToolBanner.png"
+				/>
+				<ServicesToolsCard
+					title="Inventory Tracking & Management"
 					description="Keep full control of your reselling operation with our automated inventory tracker. Monitor your stock levels, current orders, past sales, and purchase history to make informed decisions and streamline your business operations."
 					disclaimer={null}
-					image="https://i.imgur.com/vNFMFBR.png"
+					image="/InventoryTrackingToolBanner.png" 
 				/>
 				<ServicesToolsCard
-					title="Financial Hub"
-					description="Manage your reselling profits and expenses with ease using our Financial Hub. Track your earnings, costs, and generate detailed financial reports, including tax summaries, to keep your business financially organized."
+					title="Order & Shipping Automation"
+					description="Automatically generate shipping labels, sync tracking with eBay, and notify buyers. AI recommends the best shipping options based on speed and cost."
 					disclaimer={null}
-					image="https://i.imgur.com/r5viQMS.png"
+					image="/OrderAutomationToolBanner.png"
 				/>
 				<ServicesToolsCard
-					title="Store Automation"
-					description="Automate your entire reselling process from product listings to order fulfillment. With integrated tools for eBay and Amazon, our system manages your store&apos;s operations, so you can focus on scaling your business."
+					title="Returns & Refund Management"
+					description="Easily manage returns with automated tracking, inventory updates, and refund processing. AI helps reduce return rates by analyzing product performance and pricing issues."
 					disclaimer={null}
-					image="https://i.imgur.com/hnTZgcx.png"
+					image="/RefundAutomationToolBanner.png"
 				/>
 				<ServicesToolsCard
-					title="Reseller News"
-					description="Stay ahead of the competition with real-time updates and insights from the reselling world. Our Reseller News keeps you informed about market trends, product releases, and exclusive deals that can boost your profits."
+					title="Financial Hub & Profit Tracking"
+					description="Track your revenue, profits, and fees in real time. AI-driven reports provide insights into best-selling products, expenses, even managing your taxes, and sales trends to help you scale & automate your business."
 					disclaimer={null}
-					image="https://i.imgur.com/IwQsPG4.png"
+					image="/FinancialHubToolBanner.png"
 				/>
 				<ServicesToolsCard
-					title="Masterclass Courses"
-					description="Level up your reselling expertise with our in-depth Masterclass courses. Designed for beginners and experts alike, these courses provide extremely valuable insights and strategies that can be utilized no matter what, including the ins-and-outs of our systems."
+					title="Account Management & Store Settings"
+					description="Manage all aspects of your eBay store in one place, from messaging & communicating with buyers and store policies to performance tracking and automation settings."
 					disclaimer={null}
-					image="https://i.imgur.com/G5FcJf4.png"
+					image="/AccountAutoManageToolBanner.png"
 				/>
 				<ServicesToolsCard
-					title="In-Store Monitors"
-					description="Get real-time updates on in-store stock from major retailers like Tesco, Nike Outlets, Costco, and more. Our in-store monitors track inventory levels, helping you spot local deals that can be resold quickly for a profit & in bulk."
+					title="Multi-Account & Business Insights"
+					description="Oversee multiple eBay accounts within a single dashboard, compare sales performance, and apply AI-driven automation across all stores."
 					disclaimer={null}
-					image="https://i.imgur.com/wWTl7Yd.png"
-				/>
-				<ServicesToolsCard
-					title="Exclusive Inner-Circle"
-					description="Join our Exclusive Inner-Circle in the Discord Community for insider access to advanced tools, expert advice, and a supportive community of top-tier resellers. Collaborate, learn, and take your reselling business to the next level."
-					disclaimer={null}
-					image="https://i.imgur.com/EPRnIHa.png"
+					image="/MultiAccountToolBanner.png"
 				/>
 			</div>
 		</div>

ec2bfb91a3863e2f44c08a6c3ea70570a25cffe9
RoyalGr4pe <njames.programming@gmail.com>
2025-03-27 14:18:14 +0000

    disallow signup

--- src/app/l/sign-up/page.tsx
+++ src/app/l/sign-up/page.tsx
@@ -4,6 +4,7 @@ import ThemeSetter from "@/app/components/ThemeSetter";
 import Loading from "@/app/components/Loading";
 import MetadataHead from "@/app/components/MetadataHead";
 import SignUpContent from "../components/sign-up/SignUpContent";
+import { Lato, Inter } from 'next/font/google';
 
 const root = process.env.ROOT as string;
 
@@ -38,6 +39,10 @@ export const metadata = {
     },
 };
 
+
+const lato = Lato({ weight: '900', style: 'italic', subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
+
 const SignUp = () => {
     return (
         <>
@@ -45,7 +50,16 @@ const SignUp = () => {
             <ThemeSetter theme="dark" />
             <Suspense fallback={<Loading />}>
                 <Layout>
-                    <SignUpContent />
+                    <div className="flex flex-col items-center pt-20">
+                        <div className="w-full animate-fadeInPrimary">
+                            <p className={`${lato.className} text-6xl text-white text-center`}>
+                                Coming <span className="bg-gradient-to-tr pb-1 pl-1 from-textGradStart to-textGradEnd bg-clip-text text-transparent">Soon</span>
+                            </p>
+                        </div>
+                        <p className={`w-full mt-3 mb-8 pb-1 pt-2 text-gray-300 text-lg text-center animate-fadeInSecondary font-medium ${inter.className}`}>
+                            Currently Under Development - Stay Tuned!
+                        </p>
+                    </div>
                 </Layout>
             </Suspense>
         </>

e1627204458c425ee5f271c4f61a385263e76ffb
Popcorn <153072396+PopcornDev1@users.noreply.github.com>
2025-04-10 10:27:08 +0100

    updated for bunny cdn

--- src/app/l/components/home/HomeVideoShowcase.tsx
+++ src/app/l/components/home/HomeVideoShowcase.tsx
@@ -3,7 +3,7 @@ export default function HomeVideoShowcase() {
         <div className="relative w-full max-w-7xl mx-auto">
             <div className="aspect-video w-full rounded-lg overflow-hidden select-none pointer-events-none">
                 <iframe
-                    src="https://www.youtube.com/embed/bwlF5wHMFuE?controls=0&showinfo=0&rel=0&modestbranding=0&autoplay=1&mute=1&loop=1&playlist=bwlF5wHMFuE"
+                    src="https://flippify.b-cdn.net/OptimisedLandingDemo.mp4"
                     className="w-full h-full scale-[1.07]"
                     allow="autoplay; encrypted-media"
                     allowFullScreen

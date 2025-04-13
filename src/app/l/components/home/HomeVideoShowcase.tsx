89363ba2fb64eea3ab1e84106ec3e6770bb338d2
RoyalGr4pe <njames.programming@gmail.com>
2025-04-13 17:51:51 +0100

    update borders

--- src/app/l/components/home/HomeVideoShowcase.tsx
+++ src/app/l/components/home/HomeVideoShowcase.tsx
@@ -1,10 +1,10 @@
 export default function HomeVideoShowcase() {
     return (
-        <div className="relative w-full max-w-7xl mx-auto p-3 border-[3px] border-white rounded-4xl bg-white shadow-md">
-            <div className="aspect-video w-full rounded-3xl overflow-hidden select-none pointer-events-none">
+        <div className="relative w-full max-w-7xl mx-auto ">
+            <div className="aspect-video w-full p-2 border-2 border-white rounded-4xl bg-gray-100/80 shadow-md overflow-hidden select-none pointer-events-none">
                 <video
                     src="https://flippify.b-cdn.net/OptimisedLandingDemo.mp4"
-                    className="w-full h-full"
+                    className="w-full h-full rounded-3xl object-fill"
                     autoPlay
                     loop
                     muted

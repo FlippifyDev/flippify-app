b033ac076fb61dc3a29aeca64b3ff9bb34f9380d
RoyalGr4pe <njames.programming@gmail.com>
2025-05-29 17:25:50 +0100

    fix currency config

--- src/app/u/components/home/profile/ConnectAccounts/ConnectAccount.tsx
+++ src/app/u/components/home/profile/ConnectAccounts/ConnectAccount.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import AmazonConnectButton from "./AmazonConnectButton";
 import EbayConnectButton from "./EbayConnectButton";
 import ShopifyConnectButton from "./ShopifyConnectButton";
-import DiscordConnectButton from './DiscordConnectButton';
 import ConnectButton from './ConnectButton';
 
 interface ConnectAccountProps {
@@ -17,8 +16,10 @@ const ConnectAccount: React.FC<ConnectAccountProps> = ({ name, description, imag
     return (
         <div className="col-span-1 flex flex-row items-center p-4 gap-4">
             {/* Account logo */}
-            <div className="border-[3px] rounded-full p-2">
-                <Image alt={`${name} logo`} src={image} width={30} height={30} loading="lazy" />
+            <div>
+                <div className="border-[3px] rounded-full p-2 w-12 h-12 flex justify-center items-center">
+                    <Image alt={`${name} logo`} src={image} width={30} height={30} loading="lazy" className="rounded-full object-cover" />
+                </div>
             </div>
 
             {/* Account name */}
@@ -30,10 +31,10 @@ const ConnectAccount: React.FC<ConnectAccountProps> = ({ name, description, imag
             <div className='flex justify-end'>
                 {/* Connect/Disconnect Button */}
                 {name === "eBay" && <EbayConnectButton />}
+                {name === "StockX" && <ConnectButton unavailable={true} />}
                 {name === "Depop" && <ConnectButton unavailable={true} />}
                 {name === "Amazon" && <AmazonConnectButton />}
                 {name === "Shopify" && <ShopifyConnectButton />}
-                {name === "Discord" && <DiscordConnectButton />}
             </div>
         </div>
     );
